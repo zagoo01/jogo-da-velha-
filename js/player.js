6000126b17ab81e45d961d6d770fef6576ec3555
zagoo01 <jcarletti55@gmail.com>
2026-04-30 21:30:03 -0300

    Create Player class to represent a player

---
+++ js/player.js
@@ -0,0 +1,11 @@
+/**
+ * Player Class - Representa um jogador (Humano ou IA).
+  */
+  export class Player {
+      constructor(symbol, name) {
+              this.symbol = symbol;
+                      this.name = name;
+                              this.score = 0;
+                                      this.isAI = false;
+                                          }
+                                          }
